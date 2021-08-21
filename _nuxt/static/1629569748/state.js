aeb9c2267b5d5e1c086a4aead27c7d744d8c8979
Stanley <angstanley68@gmail.com>
2021-08-22 01:17:37 +0700

    Deploy new site

---
+++ _nuxt/static/1629569748/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629569748",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"27185fa2"}}}}(null,"\u002F"));
